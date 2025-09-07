01d0959cf68b95436591d768ab2eb060f10318f9
naohiko560 <private.mail.nao@gmail.com>
2025-09-07 12:32:41 +0900

    forChildのロジックを変数にまとめる

--- src/app/awzk1200/awzk1200.module.ts
+++ src/app/awzk1200/awzk1200.module.ts
@@ -1,28 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { Awzk1200Component } from './awzk1200.component';
 import { Awzk1200aComponent } from './awzk1200a/awzk1200a.component';
 import { Awzk1200bComponent } from './awzk1200b/awzk1200b.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: Awzk1200Component,
+    children: [
+      { path: 'a', component: Awzk1200aComponent },
+      { path: 'b', component: Awzk1200bComponent },
+      { path: '', redirectTo: 'a', pathMatch: 'full' },
+    ],
+  },
+];
+
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: Awzk1200Component,
-        children: [
-          { path: 'a', component: Awzk1200aComponent },
-          { path: 'b', component: Awzk1200bComponent },
-          { path: '', redirectTo: 'a', pathMatch: 'full' }
-        ]
-      }
-    ]),
+    RouterModule.forChild(routes),
     Awzk1200Component,
     Awzk1200aComponent,
-    Awzk1200bComponent
-  ]
+    Awzk1200bComponent,
+  ],
 })
 export class Awzk1200Module {}
