6204595d817246ff2e3f93ffa05d76dc3d441554
naohiko560 <private.mail.nao@gmail.com>
2025-09-07 11:27:17 +0900

    初期ルートの変更

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,6 +9,6 @@ export const routes: Routes = [
 		path: 'awzk1300',
 		loadChildren: () => import('./awzk1300/awzk1300.module').then(m => m.Awzk1300Module)
 	},
-	// { path: '', redirectTo: 'awzk1200', pathMatch: 'full' },
-	{ path: '', redirectTo: 'awzk1300', pathMatch: 'full' },
+	{ path: '', redirectTo: 'awzk1200', pathMatch: 'full' },
+	// { path: '', redirectTo: 'awzk1300', pathMatch: 'full' },
 ];
